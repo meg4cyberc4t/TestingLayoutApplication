aca1737b143ba628e4bfd502e4b26ae5aa8b631a
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-02-13 00:08:52 +0300

    Update theme_data_service.dart

--- lib/theme_data_service.dart
+++ lib/theme_data_service.dart
@@ -1,64 +1,19 @@
 import 'package:flutter/material.dart';
 
 mixin ThemeDataService {
-  // It may come in handy
-  // ThemeData (https://api.flutter.dev/flutter/material/ThemeData-class.html)
-  // ColorScheme (https://api.flutter.dev/flutter/material/ColorScheme-class.html)
+  static ThemeData get themeData => mainThemeData;
 
-  static ThemeData get themeData => customThemeData;
+  static ThemeData mainThemeData = ThemeData.from(colorScheme: colorScheme);
 
-  /// Classic ThemeData
-  ///
-  /// They are by default and there is no need to designate colors
-  static ThemeData classicDarkThemeData = ThemeData.dark();
-  static ThemeData classicLightThemeData = ThemeData.light();
-
-  /// ThemeData.from
-  ///
-  /// Makes ThemeData based on your ColorScheme
-  // So a darker color scheme is possible
-  static ThemeData colorSchemeThemeData1 =
-      ThemeData.from(colorScheme: const ColorScheme.dark());
-  // Or get a contrasting white
-  static ThemeData colorSchemeThemeData2 =
-      ThemeData.from(colorScheme: const ColorScheme.light());
-
-  static ThemeData colorSchemeThemeData3 =
-      ThemeData.from(colorScheme: const ColorScheme.highContrastDark());
-  static ThemeData colorSchemeThemeData4 =
-      ThemeData.from(colorScheme: const ColorScheme.highContrastLight());
-
-  // You can also make a ThemeData based on Color
-  static ThemeData localThemeData = ThemeData.from(
-    colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepOrange),
-  );
-
-  // The second option (Preferred!)
-  static ThemeData localThemeData2 = ThemeData.from(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange));
-
-  // So for example the themes from Notify (https://github.com/meg4cyberc4t/notify)
-  static ThemeData darkThemeDataNotify = ThemeData.from(
-      colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6EC6CA), brightness: Brightness.dark));
-  static ThemeData lightThemeDataNotify = ThemeData.from(
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8474A1)));
-
-  // Well, you can always make your own clean ThemeData
-  static ThemeData customThemeData = ThemeData(
-    // Define the default brightness and colors.
-    // brightness: Brightness.dark,
-    primarySwatch: Colors.red,
-
-    // Define the default font family.
-    fontFamily: 'Georgia',
-
-    // Define the default `TextTheme`. Use this to specify the default
-    // text styling for headlines, titles, bodies of text, and more.
-    textTheme: const TextTheme(
-      headline1: TextStyle(fontWeight: FontWeight.bold),
-      headline6: TextStyle(fontStyle: FontStyle.italic),
-      bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-    ),
+  static const color1 = Color(0xFF808080);
+  static const color2 = Color(0xFFC4C4C4);
+  static const color3 = Color(0xFF454545);
+  static const ColorScheme colorScheme = ColorScheme.dark(
+    background: color1,
+    onBackground: Colors.black,
+    primary: color2,
+    onPrimary: Colors.black,
+    tertiary: color3,
+    onTertiary: Colors.black,
   );
 }
